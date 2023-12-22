bdbe5ce4eb3267d33b1675341e7f0d74d4259776
Amr Ezzat <153765853+amreahmed@users.noreply.github.com>
2023-12-23 00:39:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.span1', {
+    strings: ["Enginnering student.", "Full-stack developer.","Freelancer."],
+    typeSpeed: 50,
+    loop:true,
+    backSpeed: 50,
+    backDelay:1000
+  });
+  
